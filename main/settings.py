97897a228f673efafa5cb096353582f2cae0dff7
jayson7 <lexxiijoo70@gmail.com>
2022-05-11 18:19:58 +0100

    config

--- main/settings.py
+++ main/settings.py
@@ -29,6 +29,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'apps',
+    'authentication',
+    'rest_framework.authtoken',
     'djoser',
     
 ]
@@ -115,3 +117,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
+DJOSER = {
+    "USER_ID_FIELD": "username"
+}
