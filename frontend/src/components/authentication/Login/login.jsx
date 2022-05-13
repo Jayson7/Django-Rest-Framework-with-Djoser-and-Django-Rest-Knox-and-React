0ef8dd6616b09c5844b3c0fc3c402b3cd69a9aa3
jayson7 <lexxiijoo70@gmail.com>
2022-05-13 12:58:45 +0100

    authentication - login

--- frontend/src/components/authentication/Login/login.jsx
+++ frontend/src/components/authentication/Login/login.jsx
@@ -1,21 +1,72 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Form } from 'react-bootstrap'
+import { useReducer } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import axios from 'axios'
+
 const LoginAuth = () => {
+  const initialState = {
+    username: '',
+    password: '',
+  }
+
+  function monitorReducer(regdata, { field, value }) {
+    return {
+      ...regdata,
+      [field]: value,
+    }
+  }
+  const [regdata, dispatch] = useReducer(monitorReducer, initialState)
+  const onchange = (e) => {
+    dispatch({ field: e.target.name, value: e.target.value })
+  }
+  const Handlesubmit = (event) => {
+    event.preventDefault()
+    console.log(regdata)
+    axios
+      .post('http://localhost:8000/api/auth/login', regdata)
+      .then((res) => {
+        console.log(res.data)
+        if (res.data.success) {
+          window.location.href = '/'
+        } else {
+          alert(res.data.message)
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+  const { username, password } = regdata
   return (
     <div>
       <div className="container">
         <div className="row">
           <div className="col-md-6 offset-md-3 my-5">
             <h1 className="text-center">Login</h1>
-            <Form>
-              <Form.Group controlId="formBasicUsername" className="my-3">
+            <Form onSubmit={Handlesubmit}>
+              <Form.Group className="my-3">
                 <Form.Label>Username</Form.Label>
-                <Form.Control type="text" placeholder="Enter username" />
+                <Form.Control
+                  type="text"
+                  placeholder="Enter username"
+                  value={username}
+                  onChange={onchange}
+                  name="username"
+                  required
+                />
               </Form.Group>
-              <Form.Group controlId="formBasicPassword" className="my-3">
+              <Form.Group className="my-3">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={onchange}
+                  required
+                  name="password"
+                />
               </Form.Group>
 
               <button className="btn btn-primary btn-block">Login</button>
