0b5f20a5f7a23759c1eebe850997af6fee216864
jayson7 <lexxiijoo70@gmail.com>
2022-05-17 13:20:19 +0100

    login process done, fetch products and its categories

--- frontend/src/components/404/404.jsx
+++ frontend/src/components/404/404.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
+
+import './notfound.css'
+import a404 from '../../images/404a.jpg'
+import b404 from '../../images/404b.jpg'
+
 function NotFound() {
+  const images = [a404, b404]
+  const randomImage = images[Math.floor(Math.random() * images.length)]
+
   return (
     <div>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 my-3 text-center mt-5">
-            <h1>404</h1>
-            <h5> The page you requested for does not exist on put App </h5>
-          </div>
-        </div>
+      <div className="notfoundImg">
+        <img className="r" src={randomImage} alt="" />
+        <h1 className="ts">404</h1>
+        <h5 className="t">
+          {' '}
+          The page you requested for does not exist on put App{' '}
+        </h5>
       </div>
     </div>
   )
