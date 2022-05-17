c95a9e8fe8794e1720bf0502eae499126c247964
jayson7 <lexxiijoo70@gmail.com>
2022-05-17 13:52:27 +0100

    login process done, fetch products and its categories

--- frontend/src/components/404/404.jsx
+++ frontend/src/components/404/404.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './notfound.css'
 import a404 from '../../images/404a.jpg'
 import b404 from '../../images/404b.jpg'
@@ -14,9 +15,13 @@ function NotFound() {
         <img className="r" src={randomImage} alt="" />
         <h1 className="ts">404</h1>
         <h5 className="t">
-          {' '}
           The page you requested for does not exist on put App{' '}
         </h5>
+        <div className="tbtn offset-5 ">
+          <button className="btn btn-success py-2 px-5 mx-5 mt-3 ">
+            Guide Me
+          </button>
+        </div>
       </div>
     </div>
   )
