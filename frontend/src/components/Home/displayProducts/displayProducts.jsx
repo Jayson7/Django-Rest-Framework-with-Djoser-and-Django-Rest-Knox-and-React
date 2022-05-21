0e91381be1ccc21dc2347dff874e0d11e174363f
jayson7 <lexxiijoo70@gmail.com>
2022-05-21 23:32:48 +0100

    products

--- frontend/src/components/Home/displayProducts/displayProducts.jsx
+++ frontend/src/components/Home/displayProducts/displayProducts.jsx
@@ -2,38 +2,51 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import axios from 'axios'
+// import { useEffect } from 'react'
 function DisplayProducts() {
-  const authCheck = useSelector((state) => state.authStore.username)
+  // const authCheck = useSelector((state) => state.authStore.username)
   const tokenCheck = useSelector((state) => state.authStore.token)
-  const products = useSelector((state) => state.productsStore.products)
-  const dispatch = useDispatch()
+  const products = useSelector((state) => state.products)
 
-  axios
-    .get('http://localhost:8000', {
-      headers: {
-        Authorization: `Token ${tokenCheck}`,
-      },
-    })
-    .then((res) => {
-      dispatch({
-        type: 'STORE_PRODUCT',
-        payload: {
-          ...res.data,
-        },
-      })
-    })
-    .catch((err) => {})
+  console.log(products)
+  tokenCheck
+    ? axios
+        .get('http://localhost:8000/', {
+          headers: {
+            Authorization: `Token ${tokenCheck}`,
+          },
+        })
+        .then((res) => {
+          console.log(res)
+        })
+
+        .catch((err) => {
+          console.log(err)
+        })
+    : console.log('no token')
 
   return (
     <div>
-      {authCheck === 'anonymous' ? (
+      {tokenCheck ? (
+        <div>
+          {products.map((product) => (
+            <div className="card" key={product.id}>
+              <img className="card-img-top" src={product.image} alt="" />
+              <div className="card-body">
+                <h5 className="card-title"> {product.name} </h5>
+                <p className="card-text"> {product.description} </p>
+                <p className="card-text">
+                  <small className="text-muted"> {product.price} </small>
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : (
         <p className="text-info">
           Sign in or Create an Account to view latest products
         </p>
-      ) : (
-        
-        
-      )}
+      )}{' '}
     </div>
   )
 }
