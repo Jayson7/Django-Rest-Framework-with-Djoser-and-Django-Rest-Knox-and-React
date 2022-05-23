3898fc47a5ca7eb7e1ebd63c3e122eb4d00614ba
jayson7 <lexxiijoo70@gmail.com>
2022-05-23 22:09:39 +0100

    display products

--- frontend/src/components/Home/slider/slider.jsx
+++ frontend/src/components/Home/slider/slider.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Carousel } from 'react-bootstrap'
 import a1 from '../../../images/slider/a1.jpg'
 import b from '../../../images/slider/2.jpg'
 import c from '../../../images/slider/3.jpg'
@@ -9,7 +10,39 @@ import { useSelector } from 'react-redux'
 
 function Slider() {
   const slidesWithAnimation = () => {
-    return <div>{/* carousel */}</div>
+    return (
+      <div className="container p-5">
+        {/* carousel */}
+
+        <Carousel>
+          <Carousel.Item>
+            <img className="d-block w-100 " src={a1} alt="First slide" />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={c} alt="Second slide" />
+
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={b} alt="Third slide" />
+
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
+    )
   }
 
   return (
