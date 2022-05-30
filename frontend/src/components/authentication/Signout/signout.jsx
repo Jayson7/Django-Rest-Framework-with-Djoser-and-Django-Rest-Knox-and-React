dbd8b6d83f9e06fa9c8c84252ed696dc7e33e318
jayson7 <lexxiijoo70@gmail.com>
2022-05-30 22:53:31 +0100

    logout bug

--- frontend/src/components/authentication/Signout/signout.jsx
+++ frontend/src/components/authentication/Signout/signout.jsx
@@ -1,33 +1,33 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import axios from 'axios'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function SignoutAuth() {
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    const authCheck = useSelector((state) => state.authStore.username)
-    const tokenCheck = useSelector((state) => state.authStore.token)
-    if (authCheck === 'anonymous') {
-        navigate('/login')
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const authCheck = useSelector((state) => state.authStore.username);
+    const tokenCheck = useSelector((state) => state.authStore.token);
+    if (authCheck === "anonymous") {
+        navigate("/login");
     } else {
         axios
-            .post('http://localhost:8000/auth/token/logout', {
+            .post("http://localhost:8000/auth/token/logout", {
                 headers: {
                     Authorization: `Token ${tokenCheck}`,
                 },
             })
             .then((res) => {
                 dispatch({
-                    type: 'LOGOUT',
-                })
-                window.location.reload()
-                navigate('/login')
+                    type: "LOGOUT",
+                });
+                // navigate("/login");
+                window.location.replace("/login");
             })
             .catch((err) => {
-                console.log(err)
-            })
+                console.log(err);
+            });
     }
 }
 
-export default SignoutAuth
+export default SignoutAuth;
